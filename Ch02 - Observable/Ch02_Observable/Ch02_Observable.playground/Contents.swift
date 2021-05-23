341fe80b6f2a1c76d89d2a350b59b8d05fccb484
pingu <jaicoco8@gmail.com>
2021-05-23 14:38:23 +0900

    Ch02 Challenage Update

--- Ch02 - Observable/Ch02_Observable/Ch02_Observable.playground/Contents.swift
+++ Ch02 - Observable/Ch02_Observable/Ch02_Observable.playground/Contents.swift
@@ -68,3 +68,106 @@ example(of: "range") {
             print(fibonacci)
         })
 }
+
+example(of: "dispose") {
+    let observable = Observable.of("A", "B", "C")
+    let subscription = observable.subscribe { event in
+        print(event)
+    }
+    subscription.dispose()
+}
+
+example(of: "DisposeBag") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("A", "B", "C")
+        .subscribe {
+            print($0)
+        }
+        .disposed(by: disposeBag)
+}
+
+example(of: "create") {
+    enum MyError: Error {
+        case anError
+    }
+    let disposeBag = DisposeBag()
+    Observable<String>.create { observer in
+        observer.onNext("1")
+        observer.onError(MyError.anError)
+        observer.onCompleted()
+        observer.onNext("?")
+        return Disposables.create()
+    } .subscribe (
+        onNext: { print($0) },
+        onError: { print($0) },
+        onCompleted: { print("Completed") },
+        onDisposed: { print("Disposed") }
+    ) .disposed(by: disposeBag)
+}
+
+// Observable Factory
+example(of: "deferred") {
+    let disposeBag = DisposeBag()
+    
+    var flip = false
+    let factory: Observable<Int> = Observable.deferred {
+        flip.toggle()
+        if flip {
+            return Observable.of(1,2,3)
+        } else {
+            return Observable.of(4,5,6)
+        }
+    }
+    
+    for _ in 0..<4 {
+        factory.subscribe(
+            onNext: {
+                print($0, terminator:"")
+            })
+            .disposed(by: disposeBag)
+        print()
+    }
+}
+
+example(of: "Single") {
+    let disposeBag = DisposeBag()
+    
+    enum FileReadError: Error {
+        case fileNotFound, unreadable, encodingFailed
+    }
+    func loadText(from name: String) -> Single<String> {
+        return Single.create { single in
+            let disposable = Disposables.create()
+            print(Bundle.main.bundlePath)
+            guard let path = Bundle.main.path(forResource: name, ofType: "swift") else {
+                single(.error(FileReadError.fileNotFound))
+                return disposable
+            }
+            
+            guard let data = FileManager.default.contents(atPath: path) else {
+                single(.error(FileReadError.unreadable))
+                return disposable
+            }
+            
+            guard let contents = String(data: data, encoding: .utf8) else {
+                single(.error(FileReadError.encodingFailed))
+                return disposable
+            }
+            
+            single(.success(contents))
+            return disposable
+        }
+    }
+    
+    loadText(from: "test")
+        .subscribe {
+            switch $0 {
+            case .success(let string):
+                print(string)
+            case .error(let error):
+                print(error)
+            }
+        }
+        .disposed(by: disposeBag)
+}
--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 Raywenderlich의 Reactive Programming with Swift를 읽으며 실습한 코드와 각 장의 Challenge를 해결하는 코드를 정리
 
-# 참고 자료
-[Raywenderlich](https://www.raywenderlich.com/books/rxswift-reactive-programming-with-swift/v4.0/)
+# RxSwift: Reactive Programming with Swift: Materials
 
-# 개인 Blog
+This repo contains all the downloadable materials and projects associated with the **[RxSwift: Reactive Programming with Swift](https://store.raywenderlich.com/products/rxswift)** from [raywenderlich.com](https://www.raywenderlich.com).
+
+Each edition has its own branch, named `editions/[EDITION]`. The default branch for this repo is for the most recent edition.
+
+## Release History
+
+| Branch                                                                           | Edition | Release Date |
+| -------------------------------------------------------------------------------- |:-------:|:------------:|
+| [editions/4.0](https://github.com/raywenderlich/rxs-materials/tree/editions/4.0) | 4.0     | 2020-08-11   |
+
+
+# 개인 Blog 정리
